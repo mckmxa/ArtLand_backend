c61ed78f4a7126028505eeada18d80f721029550
miksa <miksamaciek1@gmail.com>
2020-12-08 02:35:41 +0100

    Products and orders routes + auth with validation

---
+++ server.js
@@ -0,0 +1,29 @@
+/* express module server */
+
+const express = require('express')
+const app = express();
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './.env' })
+const port = process.env.DATABASE_PORT || 3000
+
+// wstepny router
+const productsRoute = require('./routes/products')
+const orderRoute = require('./routes/orders')
+const authRoute = require('./routes/authentication')
+
+app.use(express.json()) // w new order zeby moc pobierac dane z req.body 
+
+app.use('/api/products', productsRoute)
+app.use('/api/orders', orderRoute)
+app.use('/api/auth', authRoute)
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+  })
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+})
+
+module.exports = app
