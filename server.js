7ea9a533185b4e0b08d8b5c372926ae02c00dc6a
miksa <miksamaciek1@gmail.com>
2020-12-19 19:15:39 +0100

    Users listing, change user data (profile page)

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ const port = process.env.DATABASE_PORT || 3000
 const productsRoute = require('./routes/products')
 const orderRoute = require('./routes/orders')
 const authRoute = require('./routes/authentication')
+const usersRoute = require('./routes/users')
 
 app.use(express.json()) // w new order zeby moc pobierac dane z req.body 
 
 app.use('/api/products', productsRoute)
 app.use('/api/orders', orderRoute)
 app.use('/api/auth', authRoute)
+app.use('/api/users',usersRoute)
 
 app.get('/', (req, res) => {
     res.send('Server is running')
